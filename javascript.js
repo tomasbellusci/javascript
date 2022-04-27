e2272522e10f548dc58b9d2643c5af7525f19c19
tomasBellusci <tomasbellusci00@gmail.com>
2022-04-26 23:41:30 -0300

    desafio obligatorio 1

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
+/* 
 
 let usuario
-
+DESAFIO COMPLEMENTARIO 1
 do{
     usuario = parseFloat(prompt("Cuantas Libertadores tiene tu equipo?"))
     console.log(usuario)
@@ -24,6 +25,86 @@ do{
 
 console.log(usuario) 
 
+|| 
+
+
+let usuario
+
+do{
+    usuario = parseInt(prompt("Escriba 1 para comprar suplementos. Escriba 2 para comprar anabolicos"))
+    console.log(usuario)
+
+    if(usuario != 1 &&  usuario != 2 ){
+        alert("Ingrese números válidos por favor.")
+    }
+
+    if(usuario == 1){
+        alert("Usted quiere comprar suplementos")
+    }
+
+    if(usuario == 2){
+        alert("Usted quiere comprar anabolicos")
+    }
+}while(isNaN(usuario))
+
+console.log(usuario)*/ 
+
+
+
+let entradas
+let cantidad
+const valorBoc = 200
+const valorRiv = 150
+const valorInd = 180
+let costoInd = cantidad * valorInd
+let costoBoc = cantidad * valorBoc
+let costoRiv = entradasRiv
+
+function entradasInd (cantidad, valorInd){
+    return cantidad * valorInd
+}
+
+function entradasRiv (cantidad, valorRiv){
+    return cantidad * valorRiv
+}
+
+function entradasBoc (cantidad, valorBoc){
+    return cantidad * valorBoc
+}
+
+do {
+    entradas = prompt("queres comprar entradas para ver a independiente, river o boca ? ")
+
+    console.log("1 " + entradas);
+
+    // Condicion si le dan cancelar.
+    if (entradas == null) {
+        break;
+    }
+
+    if (entradas == "river" || entradas == "boca" || entradas == "independiente") {
+        cantidad = parseFloat(prompt("Valido! cuantas entradas queres compar ? "))
+            // .. continuar
+    } else {
+        alert("equipo no válido");
+        console.log("2 " + entradas);
+    }
+
+    if(entradas == "river"){
+        alert(entradasRiv(cantidad,valorRiv))
+    }
+
+    if(entradas == "independiente"){
+        alert(entradasInd(cantidad,valorInd))
+    }
+
+    if(entradas == "boca"){
+        alert(entradasBoc(cantidad,valorBoc))
+    }
+
+} while (entradas != "river" || entradas != "boca" || entradas != "independiente")
+
+console.log("saliste");
 
 
 
